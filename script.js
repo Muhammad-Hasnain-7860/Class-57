5c395d8c8538ec1e1646a123779ec728f2df1130
Muhammad Hasnain <muhammadhasnain7950@gmail.com>
2026-02-16 14:27:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+// const student = {
+//     name : 'Muhammad_Hasnain',
+//     age : 16,
+//     city : 'Sukkur',
+// }
+
+// console.log(student.name);
+// console.log(student['age']);
+
+// const employees = [
+//     {
+//         name : 'Hasnain',
+//         departmen : 'Hr',
+//         salary : 120000,
+//     },
+//     {
+//         name : 'Bilal',
+//         departmen : 'IT',
+//         salary : 300000,
+//     },
+//     {
+//         name : 'Husain',
+//         departmen : 'IT',
+//         salary : 300000,
+//     },
+// ];
+
+// console.log(employees[1].name , employees[2].salary)
+
+
+// const product = {
+//     title : 'Iphone',
+//     price : 120000,
+//     category : '13 pro max',
+// }
+
+// for(let key in product) {
+//     console.log(product[key]);
+// }
+
+// const user = {
+//     name : 'Muhammad_Hasnain',
+//     email : 'hasnain@test.com',
+//     skill : ['Html' , 'CSS' , 'JavaScript' , 'Tailwind'],
+// }
+// console.log(user.skill[0] , user.skill[2]);
+
+// const car = {
+//     brand : 'BMW',
+//     model : '2025',
+//     color : 'Gray',
+//     year : '2024',
+// }
+
+// console.log(Object.keys(car).length)
+
+// const book = {
+//     title : 'Urdu',
+//     another : 'Poetry',
+//     price : 200,
+// }
+
+// let value = Object.values(book);
+// for(let val of value ) {
+//     console.log(val);  
+// }
+
+// const order = {
+//     id : 1,
+//     name : 'Hassan',
+//     total: 3000,
+// }
+
+// console.log(JSON.stringify(order))
+
+// let students = [12,30,40,60,70];
+
+// let ans = students.filter((val)=> {
+//     return val < 30;
+// });
+
+// console.log(ans);
